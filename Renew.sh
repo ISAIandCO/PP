87fd1f095805a68746423d58d66cf6b5dc296227
isaico <>
2022-05-30 04:54:29 +0500

    300522

--- Renew.sh
+++ Renew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp PR_pt.rls PR_pt_temp.rls
-grep -Po '"hostName":".*?",' SmartProxy-RulesBackup.json | cut -d '"' -f 4 >> PR_pt_temp.rls
+grep -Po '"hostName":".*?",' SmartProxy*.json | cut -d '"' -f 4 >> PR_pt_temp.rls
 awk '!seen[$0]++' PR_pt_temp.rls > PR_pt.rls
 base64 PR_pt.rls > PR_b64.rls
 rm PR_pt_temp.rls
