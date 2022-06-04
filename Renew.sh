afb716d6c0afc6692e89b7910742be8d70163c1d
isaico <>
2022-06-04 10:05:01 +0500

    040622

--- Renew.sh
+++ Renew.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cp PR_pt.rls PR_pt_temp.rls
+base64 -d PR_b64.rls > PR_pt_temp.rls
+#cp PR_pt.rls PR_pt_temp.rls
 grep -Po '"hostName":".*?",' SmartProxy*.json | cut -d '"' -f 4 >> PR_pt_temp.rls
 awk '!seen[$0]++' PR_pt_temp.rls > PR_pt.rls
 base64 PR_pt.rls > PR_b64.rls
 rm PR_pt_temp.rls
+echo "2ip.ru" > PR_pt.rls
