ae0b89eda4af75ad3d1fa6882cb33d3b0dcb804f
isaico <>
2022-07-05 04:08:30 +0500

    050722

--- Renew.sh
+++ Renew.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-base64 -d PR_b64.rls > PR_pt_temp.rls
-echo "PR_b64.rls.ru" >> PR_pt_temp.rls
-#cp PR_pt.rls PR_pt_temp.rls
-grep -Po '"hostName":".*?",' SmartProxy*.json | cut -d '"' -f 4 >> PR_pt_temp.rls
-awk '!seen[$0]++' PR_pt_temp.rls | base64 > PR_b64.rls
-rm PR_pt_temp.rls
-echo "PR_pt.rls.ru" > PR_pt.rls
+base64 -d PR_b64.rls > PR_pt_temp1.rls
+grep -Po '"hostName":".*?",' SmartProxy*.json | cut -d '"' -f 4 >> PR_pt_temp1.rls
+awk '!seen[$0]++' PR_pt_temp1.rls > PR_pt_temp2.rls
+nano PR_pt_temp2.rls && base64 PR_pt_temp2.rls > PR_b64.rls
+rm PR_pt_temp*
